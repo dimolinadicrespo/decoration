9445a06a5e865fac76309632e72c18f843310d05
Diego Molina <molinacrespo@gmail.com>
2020-10-27 10:56:58 +0100

    General component of image

--- src/Image.php
+++ src/Image.php
@@ -1,43 +1,27 @@
 <?php
 
+
 namespace Dimolina;
 
-class Image
+
+abstract class Image implements Drawable
 {
     protected $path;
-    private $width;
-    private $height;
-    private $grayscale;
-    private $framed;
 
-    public function __construct($path, $width = null, $height = null, $grayscale = false, $framed = false)
+    /**
+     * ImageFromJpeg constructor.
+     * @param $path
+     */
+    public function __construct($path)
     {
         $this->path = $path;
-        $this->width = $width;
-        $this->height = $height;
-        $this->grayscale = $grayscale;
-        $this->framed = $framed;
     }
 
-    public function draw()
+    /**
+     * @return mixed
+     */
+    public function getPath()
     {
-        $img = imagecreatefromjpeg($this->path);
-
-        if ($this->width && $this->height) {
-            $img = imagescale($img, $this->width, $this->height);
-        }
-
-        if ($this->grayscale) {
-            imagefilter($img, IMG_FILTER_GRAYSCALE);
-        }
-
-        if ($this->framed) {
-            for ($i = 0; $i < $this->framed; $i++) {
-                imagerectangle($img, $i, $i, imagesx($img) - $i - 1, imagesy($img) - $i - 1, imagecolorallocate($img, 0, 0, 0));
-            }
-
-        }
-
-        return $img;
+        return $this->path;
     }
 }
